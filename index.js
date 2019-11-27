1a742028ed8f154b0062634ef2bf8de7c9b4ea67
yuvalpeled <yuval@getvim.com>
2019-11-27 17:04:50 +0200

    Initial commit - working implementation without tests

---
+++ index.js
@@ -0,0 +1,31 @@
+const childProcess = require('child_process');
+const crypto = require('crypto');
+const util = require('util');
+
+const random = util.promisify(crypto.randomBytes);
+
+function execute(command, options) {
+    const env = options && options.env ? { ...process.env, ...options.env } : process.env;
+    return new Promise((resolve, reject) => {
+        const safeCommand = `set -euo pipefail\n${command}`;
+        childProcess.exec(safeCommand, { shell: '/bin/bash', env }, (error, stdout, stderr) => {
+            if (error) reject({ ...error, stderr: removeFinalNewline(stderr) });
+            else resolve(removeFinalNewline(stdout));
+        })
+    })
+}
+
+function pipe(command, options) {
+    return function (stdIn) {
+        return random(32)
+            .then(randomId => randomId.toString('hex'))
+            .then(randomId => execute(`${command} <<${randomId}\n${stdIn}\n${randomId}`, options))
+    }
+}
+
+function removeFinalNewline(stdio) {
+    return stdio.endsWith('\n') ? stdio.slice(0, -1) : stdio;
+}
+
+execute.pipe = pipe;
+module.exports = execute;
